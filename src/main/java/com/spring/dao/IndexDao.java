8a4a82389f13f420d99ffe6135c1430d69c401cf
minimyo <ghghgh1246@gmail.com>
2022-03-02 16:39:06 +0900

    [메인] 벼룩 최신글 3개 추가

--- src/main/java/com/spring/dao/IndexDao.java
+++ src/main/java/com/spring/dao/IndexDao.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.spring.dto.BoardDto;
+import com.spring.dto.FleaDto;
+import com.spring.dto.ImgDto;
 
 @Mapper
 public interface IndexDao {
 	List<BoardDto> boardbest();
+	List<FleaDto> fleabest();
+	ImgDto getImg(int imgid);
 }
--- src/main/java/com/spring/service/IndexService.java
+++ src/main/java/com/spring/service/IndexService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import com.spring.dao.IndexDao;
 import com.spring.dto.BoardDto;
+import com.spring.dto.FleaDto;
+import com.spring.dto.ImgDto;
 
 @Service
 public class IndexService {
@@ -15,5 +17,13 @@ public class IndexService {
 	
 	public List<BoardDto> boardbest(){
 		return dao.boardbest();
+	}
+	
+	public List<FleaDto> fleabest(){
+		return dao.fleabest();
+	}
+	
+	public ImgDto getImg(int imgid) {
+		return dao.getImg(imgid);
 	}
 }
